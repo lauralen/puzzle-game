e2518579b92a6c81de871e581542e84061dcd235
lauralen <laura.l@8devices.com>
2020-05-24 10:48:36 +0300

    Initialize repository

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import unsplashId from './utils/unsplashId';
 
-function App() {
+export default function App() {
+  const [randomImage, setRandomImage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  async function fetchData() {
+    setError();
+
+    try {
+      setIsLoading(true);
+
+      const res = await axios.get('https://api.unsplash.com/photos/random', {
+        headers: {
+          Authorization: `Client-ID ${unsplashId}`
+        }
+      });
+
+      setRandomImage(res.data);
+    } catch (err) {
+      setError('Oops! Something went wrong');
+    }
+    setIsLoading(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button
+        onClick={() => {
+          fetchData();
+        }}
+      >
+        Get random image
+      </button>
+      {randomImage ? (
+        <img src={randomImage.urls.regular} alt={randomImage.alt_description} />
+      ) : null}
+      {isLoading ? <p>loading...</p> : null}
+      {error ? <p>{error}</p> : null}
     </div>
   );
 }
-
-export default App;

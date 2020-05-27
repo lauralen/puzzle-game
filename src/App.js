6732aed67c29b2f69759718420905714323fe4e8
lauralen <laura.l@8devices.com>
2020-05-27 19:18:59 +0300

    Add random image fetch function

--- src/App.js
+++ src/App.js
@@ -1,45 +1,48 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import unsplashId from './utils/unsplashId';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import unsplashId from "utils/unsplashId";
+
+import Header from "./layout/Header";
 
 export default function App() {
-  const [randomImage, setRandomImage] = useState(null);
+  const [image, setImage] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
+  const [puzzleSize, setPuzzleSize] = useState({
+    height: 400,
+    width: 600,
+    pieceSize: 100
+  });
 
-  async function fetchData() {
+  useEffect(() => {
+    fetchImage();
+  }, []);
+
+  async function fetchImage() {
     setError();
 
     try {
       setIsLoading(true);
 
-      const res = await axios.get('https://api.unsplash.com/photos/random', {
-        headers: {
-          Authorization: `Client-ID ${unsplashId}`
+      const res = await axios.get(
+        `https://api.unsplash.com/photos/random?&h=${puzzleSize.height}&w=${puzzleSize.width}&fit=clamp`,
+        {
+          headers: {
+            Authorization: `Client-ID ${unsplashId}`
+          }
         }
-      });
+      );
 
-      setRandomImage(res.data);
+      setImage(res.data);
     } catch (err) {
-      setError('Oops! Something went wrong');
+      setError("Oops! Something went wrong");
+      setIsLoading(false);
     }
-    setIsLoading(false);
   }
 
   return (
-    <div>
-      <button
-        onClick={() => {
-          fetchData();
-        }}
-      >
-        Get random image
-      </button>
-      {randomImage ? (
-        <img src={randomImage.urls.regular} alt={randomImage.alt_description} />
-      ) : null}
-      {isLoading ? <p>loading...</p> : null}
-      {error ? <p>{error}</p> : null}
-    </div>
+    <>
+      <Header fetchImage={fetchImage} />
+    </>
   );
 }

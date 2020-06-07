40dfbb463fb4727c2c708d250b243e625b65bb7e
lauralen <laura.l@8devices.com>
2020-06-07 20:26:32 +0300

    Add disabled prop to button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import style from './Button.module.scss';
 
-const Button = ({ title, type, action }) => {
+const Button = ({ title, type, action, disabled }) => {
   return (
-    <button className={`button ${type && style[type]}`} onClick={action}>
+    <button
+      className={`button ${type && style[type]}`}
+      onClick={action}
+      disabled={disabled}
+    >
       {title}
     </button>
   );

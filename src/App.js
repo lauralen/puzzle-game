f9d96d043f084f9cd07436b3c26375e8e0a2696a
lauralen <laura.l@8devices.com>
2020-06-13 11:03:01 +0300

    Add load more images button

--- src/App.js
+++ src/App.js
@@ -1,72 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import unsplashId from 'utils/unsplashId';
-
-import Header from './layout/Header';
-import Gallery from './layout/Gallery';
-import GameBoard from './layout/GameBoard';
+import React, { useState } from 'react';
+import Header from 'layout/Header';
+import Gallery from 'layout/Gallery';
+import GameBoard from 'layout/GameBoard';
 
 export default function App() {
-  const [images, setImages] = useState(null);
   const [keyword, setKeyword] = useState('');
   const [selectedImage, setSelectedImage] = useState(null);
   const [useRandomImage, setUseRandomImage] = useState(null);
   const [startGame, setStartGame] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
   const [puzzleSize, setPuzzleSize] = useState({
     rows: 4,
     columns: 6,
     pieceSize: 100
   });
-  const unsplashUrl = 'https://api.unsplash.com/';
-
-  useEffect(() => {
-    fetchImages();
-  }, []);
-
-  useEffect(() => {
-    setError();
-    keyword === '' ? fetchImages() : fetchImagesByKeyword(keyword);
-  }, [keyword]);
-
-  async function fetchImages() {
-    try {
-      setIsLoading(true);
-
-      const res = await axios.get(`${unsplashUrl}photos`, {
-        params: { per_page: 5 },
-        headers: {
-          Authorization: `Client-ID ${unsplashId}`
-        }
-      });
-
-      setImages(res.data);
-      setIsLoading(false);
-    } catch (err) {
-      setError('Oops! Something went wrong');
-      setIsLoading(false);
-    }
-  }
-
-  async function fetchImagesByKeyword() {
-    try {
-      setIsLoading(true);
-
-      const res = await axios.get(`${unsplashUrl}search/photos`, {
-        params: { query: keyword, per_page: 5 },
-        headers: {
-          Authorization: `Client-ID ${unsplashId}`
-        }
-      });
-
-      setImages(res.data.results);
-      setIsLoading(false);
-    } catch (err) {
-      setError('Oops! Something went wrong');
-      setIsLoading(false);
-    }
-  }
 
   return (
     <>
@@ -79,17 +25,13 @@ export default function App() {
             setPuzzleSize={setPuzzleSize}
             keyword={keyword}
             setKeyword={setKeyword}
-            fetchImages={fetchImages}
             useRandomImage={useRandomImage}
             setUseRandomImage={setUseRandomImage}
             setStartGame={setStartGame}
             selectedImage={selectedImage}
           />
           <Gallery
-            images={images}
-            isLoading={isLoading}
-            setIsLoading={setIsLoading}
-            error={error}
+            keyword={keyword}
             selectedImage={selectedImage}
             setSelectedImage={setSelectedImage}
           />
--- src/layout/Gallery.jsx
+++ src/layout/Gallery.jsx
@@ -1,17 +1,76 @@
-import React from 'react';
-import Loader from 'components/Loader';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import unsplashId from 'utils/unsplashId';
 import style from './Gallery.module.scss';
 
-const Gallery = ({
-  images,
-  isLoading,
-  error,
-  selectedImage,
-  setSelectedImage
-}) => {
+import Loader from 'components/Loader';
+import Button from 'components/Button';
+
+const Gallery = ({ keyword, selectedImage, setSelectedImage }) => {
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState({ page: true, images: false });
+  const [error, setError] = useState();
+
+  const unsplashUrl = 'https://api.unsplash.com/';
+
+  useEffect(() => {
+    setError();
+    setImages([]);
+    setIsLoading({ ...isLoading, page: true });
+
+    setPage(1);
+    keyword === '' ? fetchImages() : fetchImagesByKeyword(keyword);
+  }, [keyword]);
+
+  useEffect(() => {
+    setError();
+    keyword === '' ? fetchImages() : fetchImagesByKeyword(keyword);
+  }, [page]);
+
+  async function fetchImages() {
+    try {
+      setIsLoading({ ...isLoading, images: true });
+
+      const res = await axios.get(`${unsplashUrl}photos`, {
+        params: { page: page, per_page: 20 },
+        headers: {
+          Authorization: `Client-ID ${unsplashId}`
+        }
+      });
+
+      setImages(images.concat(res.data));
+      setIsLoading({ images: false, page: false });
+    } catch (err) {
+      setError('Oops! Something went wrong');
+      console.log(err);
+      setIsLoading({ images: false, page: false });
+    }
+  }
+
+  async function fetchImagesByKeyword() {
+    try {
+      setIsLoading({ ...isLoading, images: true });
+
+      const res = await axios.get(`${unsplashUrl}search/photos`, {
+        params: { query: keyword, page: page, per_page: 20 },
+        headers: {
+          Authorization: `Client-ID ${unsplashId}`
+        }
+      });
+
+      setImages(images.concat(res.data.results));
+      setIsLoading({ images: false, page: false });
+    } catch (err) {
+      setError('Oops! Something went wrong');
+      console.log(err);
+      setIsLoading({ images: false, page: false });
+    }
+  }
+
   return (
     <div className={style.gallery}>
-      {isLoading ? (
+      {isLoading.page ? (
         <Loader />
       ) : error ? (
         <p>{error}</p>
@@ -22,7 +81,6 @@ const Gallery = ({
               return (
                 <div
                   key={index}
-                  className={`img ${img === selectedImage && 'selected'}`}
                   className={[
                     style.img,
                     img === selectedImage ? style.selected : null
@@ -57,6 +115,16 @@ const Gallery = ({
                 </div>
               );
             })}
+          {page <= 5 ? (
+            <Button
+              action={() => {
+                setPage(page + 1);
+              }}
+              title='Load more'
+              type='secondary'
+              disabled={isLoading.images}
+            />
+          ) : null}
         </div>
       )}
     </div>

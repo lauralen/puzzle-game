9eb584943978e951d4cd86deabadcdcc94d55df5
lauralen <laura.l@8devices.com>
2020-06-13 22:59:53 +0300

    Update inputs style

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -4,9 +4,8 @@ import style from './Checkbox.module.scss';
 const Checkbox = ({ value, name, label, onChange }) => {
   return (
     <>
-      <label className={style.label}>{label}</label>
       <input value={value} name={name} type='checkbox' onChange={onChange} />
-      <span />
+      <label className={style.label}>{label}</label>
     </>
   );
 };
